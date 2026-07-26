658b0d19dca1d6682e0efafbcf6170fa584b818d
Bruno Choupina <brunochoupina@MacBook-Pro-de-Bruno.local>
2026-07-26 18:29:18 +0200

    derniers ajustements avant publication de la plateforme

--- Presentation.py
+++ Presentation.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from utils.QIS.crypto import text
 
 #from utils.option_pricing import show as show_option_pricing
 
@@ -9,24 +10,4 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("Bruno Quant App")
-st.header("Quantitative Finance Projects")
-
-st.subheader("About Me")
-
-st.write("""
-I am currently pursuing the MSc 272 *Economics and Finance* at
-Université Paris Dauphine–PSL and completing the first internship of my
-gap year at TotalEnergies as a **Short-Term Power Middle Office
-Analyst**.
-
-This platform showcases my quantitative finance projects, combining
-personal research with interactive educational tools inspired by the
-concepts studied throughout my master's program.
-
-Its objective is both to present the results of my quantitative
-research and to provide hands-on applications of financial concepts
-through interactive visualizations, simulations and quantitative
-models.
-
-""")
+st.markdown(text.about_me)
--- utils/QIS/crypto/text.py
+++ utils/QIS/crypto/text.py
@@ -1,36 +1,15 @@
-st.title("Bruno Quant App")
-st.header("Quantitative Finance Projects")
-
-st.subheader("About Me")
-
-st.write("""
-I am currently pursuing the MSc 272 *Economics and Finance* at
-Université Paris Dauphine–PSL and completing the first internship of my
-gap year at TotalEnergies as a **Short-Term Power Middle Office
-Analyst**.
-
-This platform showcases my quantitative finance projects, combining
-personal research with interactive educational tools inspired by the
-concepts studied throughout my master's program.
-
-Its objective is both to present the results of my quantitative
-research and to provide hands-on applications of financial concepts
-through interactive visualizations, simulations and quantitative
-models."""
-
-
 about_me="""
 
-## Bruno Quant App
+# Bruno Quant App
 
 ### Quantitative Finance Projects
 
-### About Me
+#### About Me
 
 I am currently pursuing the MSc 272 *Economics and Finance* at
 Université Paris Dauphine–PSL and completing the first internship of my
-gap year at TotalEnergies as a **Short-Term Power Middle Office
-Analyst**.
+gap year at TotalEnergies as a Short-Term Power Middle Office
+Analyst.
 
 This platform showcases my quantitative finance projects, combining
 personal research with interactive educational tools inspired by the
